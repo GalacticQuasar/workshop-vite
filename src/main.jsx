da16129987a7c18ccf79b27103c85fcd48cb7b3f
Kaley Kwan <kaleymkwan@gmail.com>
2024-10-25 19:23:46 -0400

    update image

--- src/main.jsx
+++ src/main.jsx
@@ -1,28 +1,28 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import DemoPage from './pages/demoPage.jsx'
-import AkashPage from './pages/AkashPage.jsx'
-import ShreyasViswanathan from './pages/shreyasViswanathan.jsx';
+import DemoPage from "./pages/demoPage.jsx";
+import AkashPage from "./pages/AkashPage.jsx";
+import ShreyasViswanathan from "./pages/shreyasViswanathan.jsx";
 import AmeyPage from './pages/AmeyPage.jsx'
-import './index.css'
-import Home from './Home.jsx'
-import Example from './pages/example.jsx';
-import Kaley from './pages/Kaley.jsx';
+import "./index.css";
+import Home from "./Home.jsx";
+import Example from "./pages/example.jsx";
+import Kaley from "./pages/Kaley.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/example' element={<Example />} />
-        <Route path='/demo' element={<DemoPage />} />
-        <Route path='/profile/:name' element={<ProfileRouter />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/example" element={<Example />} />
+        <Route path="/demo" element={<DemoPage />} />
+        <Route path="/profile/:name" element={<ProfileRouter />} />
       </Routes>
     </Router>
-  </StrictMode>,
-)
+  </StrictMode>
+);
 
 function ProfileRouter() {
   const { name } = useParams();
@@ -32,7 +32,7 @@ function ProfileRouter() {
       return <DemoPage />;
 
     // Change <DemoPage /> to your custom profile page, and change the case to your name
-    case "Person1":
+    case "Kaley":
       return <Kaley />;
     case "Person2":
       return <DemoPage />;
@@ -47,7 +47,7 @@ function ProfileRouter() {
     case "Akash Ravandhu":
       return <AkashPage />;
     case "Person8":
-        return <DemoPage />;
+      return <DemoPage />;
     case "Person9":
       return <DemoPage />;
     case "Person10":
